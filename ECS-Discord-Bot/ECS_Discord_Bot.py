f928cb14fd6cdfc6f6f8d7ef763ec0b19d06277c
geman <geman220@gmail.com>
2024-01-17 18:04:32 -0800

    added /ticketlist command.

--- ECS-Discord-Bot/ECS_Discord_Bot.py
+++ ECS-Discord-Bot/ECS_Discord_Bot.py
@@ -40,7 +40,7 @@ flask_token = os.getenv('FLASK_TOKEN')
 discord_admin_role = os.getenv('ADMIN_ROLE')
 dev_id = os.getenv('DEV_ID')
 server_id = os.getenv('SERVER_ID')
-BOT_VERSION = "1.3.0"
+BOT_VERSION = "1.3.2"
 closed_matches = set()
 
 def initialize_db():
@@ -223,7 +223,7 @@ async def get_team_record(ctx, team_id):
     data = await send_async_http_request(ctx, url)
     if data and 'team' in data:
         record_data = data['team'].get('record', {}).get('items', [])
-        team_logo_url = data['team']['logos'][0]['href']  # Assuming the first logo is the one you want
+        team_logo_url = data['team']['logos'][0]['href']
         if record_data:
             stats = record_data[0].get('stats', [])
             record_info = {stat['name']: stat['value'] for stat in stats}
@@ -425,7 +425,7 @@ class CheckOrderModal(discord.ui.Modal):
         self.add_item(discord.ui.TextInput(label="Order ID", placeholder="Enter the order ID number"))
 
     async def on_submit(self, interaction: discord.Interaction):
-        order_id = self.children[0].value.strip()  # Retrieve the entered order ID
+        order_id = self.children[0].value.strip()
         if order_id.startswith('#'):
             order_id = order_id[1:]
 
@@ -529,7 +529,7 @@ class AdminCommands(commands.Cog, name="Admin Commands"):
         await interaction.response.send_message(f"ECS Bot - developed by <@{dev_id}> version {BOT_VERSION}")
 
     @app_commands.command(name='checkorder', description="Check an ECS membership order")
-    @app_commands.guilds(discord.Object(id=server_id))  # Replace with your server ID
+    @app_commands.guilds(discord.Object(id=server_id))
     async def check_order(self, interaction: discord.Interaction):
         # Admin role check
         if not await has_admin_role(interaction):
@@ -540,7 +540,7 @@ class AdminCommands(commands.Cog, name="Admin Commands"):
         await interaction.response.send_modal(CheckOrderModal(self.bot))
 
     @app_commands.command(name='newseason', description="Start a new season with a new ECS membership role")
-    @app_commands.guilds(discord.Object(id=server_id))  # Replace with your server ID
+    @app_commands.guilds(discord.Object(id=server_id))
     async def new_season(self, interaction: discord.Interaction):
         # Admin role check
         if not await has_admin_role(interaction):
@@ -779,18 +779,44 @@ class WooCommerceCommands(commands.Cog, name="WooCommerce Commands"):
     def __init__(self, bot):
         self.bot = bot
 
+    @app_commands.command(name='ticketlist', description="List all tickets for sale")
+    @app_commands.guilds(discord.Object(id=server_id))
+    async def list_tickets(self, interaction: discord.Interaction):
+        if not await has_admin_role(interaction):
+            await interaction.response.send_message("You do not have the necessary permissions.", ephemeral=True)
+            return
+
+        home_tickets_category = "765197885"
+        away_tickets_category = "765197886"
+
+        # Construct URLs for querying products in these categories
+        home_tickets_url = wc_url.replace('orders/', f'products?category={home_tickets_category}')
+        away_tickets_url = wc_url.replace('orders/', f'products?category={away_tickets_category}')
+
+        # Fetch products from each category
+        home_tickets = await call_woocommerce_api(interaction, home_tickets_url)
+        await asyncio.sleep(1)
+        away_tickets = await call_woocommerce_api(interaction, away_tickets_url)
+
+        # Prepare message content
+        message_content = "🏠 **Home Tickets:**\n"
+        message_content += "\n".join([product["name"] for product in home_tickets]) if home_tickets else "No home tickets found."
+
+        message_content += "\n\n🚗 **Away Tickets:**\n"
+        message_content += "\n".join([product["name"] for product in away_tickets]) if away_tickets else "No away tickets found."
+
+        # Send the response
+        await interaction.response.send_message(message_content, ephemeral=True)
+
     @app_commands.command(name='getorderinfo', description="Retrieve order details for a specific product")
     @app_commands.describe(product_title='Title of the product')
-    @app_commands.guilds(discord.Object(id=server_id))  # Replace with your server ID
+    @app_commands.guilds(discord.Object(id=server_id))
     async def get_product_orders(self, interaction: discord.Interaction, product_title: str):
         if not await has_admin_role(interaction):
             await interaction.response.send_message("You do not have the necessary permissions.", ephemeral=True)
             return
 
-        # Modify the base URL to query the products endpoint
         product_url = wc_url.replace('orders/', f'products?search={product_title}')
-
-        # Query WooCommerce for the product
         products = await call_woocommerce_api(interaction, product_url)
     
         if not products:
@@ -803,17 +829,13 @@ class WooCommerceCommands(commands.Cog, name="WooCommerce Commands"):
             await interaction.response.send_message("Product not found.", ephemeral=True)
             return
 
-        # Modify the base URL to query orders for the specific product
         orders_url = wc_url.replace('products?', f'orders?product={product["id"]}')
-
-        # Query WooCommerce for the orders
         orders = await call_woocommerce_api(interaction, orders_url)
 
         if not orders:
             await interaction.response.send_message("No orders found for this product.", ephemeral=True)
             return
 
-        # Extract data and generate CSV
         csv_output = io.StringIO()
         csv_writer = csv.writer(csv_output)
         header = ["Product Name", "Customer First Name", "Customer Last Name", "Customer Username", "Customer Email",
@@ -835,13 +857,12 @@ class WooCommerceCommands(commands.Cog, name="WooCommerce Commands"):
                            order['number'],
                            order['status'],
                            order.get('customer_note', 'N/A'),
-                           item['name']]  # Assuming this is the variation name
+                           item['name']]
                     csv_writer.writerow(row)
 
         csv_output.seek(0)
         csv_file = discord.File(fp=csv_output, filename=f"{product_title}_orders.csv")
 
-        # Send CSV File
         await interaction.response.send_message(f"Orders for product '{product_title}':", file=csv_file, ephemeral=True)
 
         csv_output.close()
