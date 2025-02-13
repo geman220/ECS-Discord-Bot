a5ee982827d1441bea3cceff972f19c675606bcc
geman <geman220@gmail.com>
2025-02-12 17:40:20 -0800

    Finally implemented search...

--- Discord-Bot-WebUI/app/__init__.py
+++ Discord-Bot-WebUI/app/__init__.py
@@ -237,6 +237,7 @@ def init_blueprints(app):
     from app.monitoring import monitoring_bp
     from app.user_api import user_bp
     from app.help import help_bp
+    from app.search import search_bp
 
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(publeague_bp, url_prefix='/publeague')
@@ -259,6 +260,7 @@ def init_blueprints(app):
     app.register_blueprint(user_bp, url_prefix='/api')
     app.register_blueprint(monitoring_bp)
     app.register_blueprint(help_bp, url_prefix='/help')
+    app.register_blueprint(search_bp)
 
 def init_context_processors(app):
     @app.context_processor
